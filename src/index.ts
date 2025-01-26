031bc661edab00e1367c5699327d708dfb4c4a39
julien14 <juxishimwe@gmail.com>
2025-01-26 23:18:23 +0200

    Creating Database Mongoose...

--- src/index.ts
+++ src/index.ts
@@ -22,3 +22,6 @@ const server = http.createServer(app);
 server.listen(8080, () => {
   console.log('Server is running on http://localhost:8080/');
 });
+
+const MONGO_URL =
+  'mongodb+srv://julish:Julish@!DB14@cluster0.2urtb.mongodb.net/';
