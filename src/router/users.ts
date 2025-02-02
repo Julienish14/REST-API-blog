c17517d7c614de2a498f3bff3314ddbabd50420e
julien14 <juxishimwe@gmail.com>
2025-02-02 23:13:59 +0200

    Get all users from Database

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import AsyncHandler from 'express-async-handler';
 
-import { getAllUsers } from 'controllers/users';
+import { getAllUsers } from '../controllers/users';
 
 export default (router: express.Router) => {
   router.get('/users', AsyncHandler(getAllUsers));
