7c9da5870612bc35606ad7ac299e03b757798ba4
julien14 <juxishimwe@gmail.com>
2025-02-03 21:02:03 +0200

    Work on middleware to check if the user has access to update

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -13,15 +13,15 @@ export const isOwner = async (
     const currentUserId = get(req, 'identity._id') as string;
 
     if (!currentUserId) {
-      return res.sendStatus(403);
+      res.sendStatus(403);
     }
     if (currentUserId.toString() !== id) {
-      return res.sendStatus(403);
-      next();
+      res.sendStatus(403);
     }
+    next();
   } catch (error) {
     console.log(error);
-    return res.sendStatus(400);
+    res.sendStatus(400);
   }
 };
 
