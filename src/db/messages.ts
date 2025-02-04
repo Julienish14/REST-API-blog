79c63be8bffd848656e2ebdf3cebbeb325d09a2f
julien14 <juxishimwe@gmail.com>
2025-02-04 15:04:09 +0200

    Work on messages Schema | Model

--- src/db/messages.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const messageSchema = new mongoose.Schema({
-  name: { type: String, require: true },
-  subject: { type: String, require: true },
-  message: { type: String, require: true },
-});
-
-export const messageModel = mongoose.model('Message', messageSchema);
