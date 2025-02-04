e408966413bd4bdbce659c6541b6e5357910f103
julien14 <juxishimwe@gmail.com>
2025-02-04 14:40:51 +0200

    Work on messages Schema | Model

--- src/db/messages.ts
+++ src/db/messages.ts
@@ -5,3 +5,5 @@ const messageSchema = new mongoose.Schema({
   subject: { type: String, require: true },
   message: { type: String, require: true },
 });
+
+export const messageModel = mongoose.model('Message', messageSchema);
