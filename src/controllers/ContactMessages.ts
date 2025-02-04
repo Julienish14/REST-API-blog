937a08c76563d85ee81067bdcd0a216ba0cf5f30
julien14 <juxishimwe@gmail.com>
2025-02-04 15:44:19 +0200

    Work on Contact Message controller (Delete Message)

--- src/controllers/ContactMessages.ts
+++ src/controllers/ContactMessages.ts
@@ -19,3 +19,21 @@ export const submitMessage = async (
     res.sendStatus(400);
   }
 };
+
+export const deleteMessage = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const deletedMessage = await ContactMessage.deleteOne({
+      _id: req.params.messageId,
+    });
+    res
+      .status(201)
+      .json({ message: 'Message Deleted Successfully!', data: deleteMessage });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+};
--- src/router/contactMessages.ts
+++ src/router/contactMessages.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 
-import { submitMessage } from '../controllers/ContactMessages';
+import { submitMessage, deleteMessage } from '../controllers/ContactMessages';
 import { isAuthenticated } from '../middlewares';
 
 export default (router: express.Router) => {
   router.post('/contacts', isAuthenticated, submitMessage);
+  router.delete('/contacts/:id', isAuthenticated, deleteMessage);
 };
