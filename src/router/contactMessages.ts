fe1d32be13124ecb922b71627fa84f78a9733e0e
julien14 <juxishimwe@gmail.com>
2025-02-05 13:56:00 +0200

    Test getting all contact messages

--- src/router/contactMessages.ts
+++ src/router/contactMessages.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 
-import { submitMessage, deleteMessage } from '../controllers/ContactMessages';
+import {
+  submitMessage,
+  getAllMessage,
+  deleteMessage,
+} from '../controllers/ContactMessages';
 import { isAuthenticated } from '../middlewares';
 
 export default (router: express.Router) => {
   router.post('/contacts', isAuthenticated, submitMessage);
   router.delete('/contacts/:id', isAuthenticated, deleteMessage);
+  router.get('/contacts', isAuthenticated, getAllMessage);
 };
