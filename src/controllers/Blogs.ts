c0904de9c75e268ddd3f22a5d7b60220534b0a51
julien14 <juxishimwe@gmail.com>
2025-02-13 16:25:00 +0200

    Blog or Article contorller -- Create article

--- src/controllers/Blogs.ts
+++ src/controllers/Blogs.ts
@@ -1,1 +1,14 @@
 import express from 'express';
+
+import BlogsArticles from 'db/Blogs';
+
+export const createBlog = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
