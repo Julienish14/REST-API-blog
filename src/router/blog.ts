538b1cc74422c634657035979e6623dba1abdc12
julien14 <juxishimwe@gmail.com>
2025-02-24 17:04:50 +0200

    fix issue with get ID

--- src/router/blog.ts
+++ src/router/blog.ts
@@ -5,5 +5,5 @@ import { isAuthenticated } from '../middlewares';
 export default (router: express.Router) => {
   router.post('/blogs', isAuthenticated, createBlog);
   router.get('/blogs', isAuthenticated, getAllPost);
-  router.get('/blogs/postId', isAuthenticated, getOnePost);
+  router.get('/blogs/:postId', isAuthenticated, getOnePost);
 };
