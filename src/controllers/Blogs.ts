f40b255e2ab9de7418d0f8b9d2fdebfbba2eabb3
julien14 <juxishimwe@gmail.com>
2025-03-02 21:08:27 +0200

    Work on Images in Blog Post

--- src/controllers/Blogs.ts
+++ src/controllers/Blogs.ts
@@ -39,7 +39,24 @@ export const createBlog = async (
     res.sendStatus(400);
   }
 };
-
+export const createBlog = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  const { title, body } = req.body;
+  try {
+    const newBlog = new BlogsArticles({ title, body });
+    await newBlog.save();
+    res.status(201).json({
+      message: 'New Blog Article created successfully!',
+      data: newBlog,
+    });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+};
 export const getAllPost = async (
   req: express.Request,
   res: express.Response,
