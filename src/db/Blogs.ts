1cf708f97356276113c1fd275a0e22c4b1a69a19
julien14 <juxishimwe@gmail.com>
2025-03-07 18:23:11 +0200

    Multiple changes in Blog schema, Add Utils folder with multer

--- src/db/Blogs.ts
+++ src/db/Blogs.ts
@@ -1,16 +1,26 @@
-import mongoose, { Schema, Document } from 'mongoose';
+// import mongoose, { Schema, Document } from 'mongoose';
 
-interface BlogPost extends Document {
-  title: String;
-  body: String;
-  createdAt: Date;
-}
+// interface BlogPost extends Document {
+//   title: String;
+//   body: String;
+//   createdAt: Date;
+// }
 
-const Blogs: Schema = new Schema({
+// const Blogs: Schema = new Schema({
+//   title: { type: String, require: true },
+//   body: { type: String, require: true },
+//   createdAt: { type: Date, default: Date.now },
+// });
+
+// const BlogsArticles = mongoose.model<BlogPost>('BlogsArticles', Blogs);
+// export default BlogsArticles;
+
+import mongoose from 'mongoose';
+
+const BlogsPostSchema = new mongoose.Schema({
   title: { type: String, require: true },
-  body: { type: String, require: true },
+  content: { type: String, require: true },
+  imageUrl: { type: String },
   createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 });
-
-const BlogsArticles = mongoose.model<BlogPost>('BlogsArticles', Blogs);
-export default BlogsArticles;
