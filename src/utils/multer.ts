8b951331602ed6c7349a97c2af4ad8ec84801490
julien14 <juxishimwe@gmail.com>
2025-03-08 19:26:11 +0200

    Cleaning codes

--- src/utils/multer.ts
+++ src/utils/multer.ts
@@ -18,30 +18,3 @@ const fileFilter = (
 const upload = multer({ storage, fileFilter });
 
 export default upload;
-
-// const storage = multer.diskStorage({
-//   destination: (req: express.Request, file, cb) => {
-//     cb(null, 'uploads/');
-//   },
-//   filename: (req: express.Request, file, cb) => {
-//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-//     cb(
-//       null,
-//       file.fieldname + '-' + uniqueSuffix + '.' + file.mimetype.split('/')[1]
-//     );
-//   },
-// });
-
-// const fileFilter = (
-//   req: express.Request,
-//   file: Express.Multer.File,
-//   cb: multer.FileFilterCallback
-// ) => {
-//   if (file.mimetype.startsWith('image/')) {
-//     cb(null, true);
-//   } else {
-//     cb(new Error('Only image files are allowed bruh 😢!'));
-//   }
-// };
-
-// export const upload = multer({ storage, fileFilter });
