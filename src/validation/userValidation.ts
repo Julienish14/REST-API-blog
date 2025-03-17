4d7015c20266be8698927f751362a9629936ce1b
julien14 <juxishimwe@gmail.com>
2025-03-17 22:43:45 +0200

    Validate users email,username fullname

--- src/validation/userValidation.ts
+++ src/validation/userValidation.ts
@@ -3,7 +3,7 @@ import { body, validationResult } from 'express-validator';
 export const validateUser = [
   body('fullname')
     .trim()
-    .isLength({ min: 3 })
+    .isLength({ min: 12 })
     .withMessage('Fullname must be at least 4 characters long')
     .isAlphanumeric()
     .withMessage('Fullname must conatin only letters'),
@@ -12,4 +12,11 @@ export const validateUser = [
     .trim()
     .isEmail()
     .withMessage('Please enter a valid email address'),
+
+  body('username')
+    .trim()
+    .isLength({ min: 6 })
+    .withMessage('Username must be at least 3 characters long')
+    .isAlphanumeric()
+    .withMessage('Username must contain only letter and number'),
 ];
