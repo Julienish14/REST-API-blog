216b665371a9afe04520eaafef0de340fc1e343e
julien14 <juxishimwe@gmail.com>
2025-03-19 16:50:30 +0200

    work on the validating blogs add body

--- src/validation/blogValidation.ts
+++ src/validation/blogValidation.ts
@@ -5,3 +5,9 @@ export const validateBlog = [
   body('title').trim().withMessage('Please Enter the title of this article'),
   body('content').trim().withMessage('Please Enter the content of the article'),
 ];
+
+async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+): Promise<void> => {};
