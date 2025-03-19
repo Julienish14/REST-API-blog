9f3c6501b2be83edfa6db17129bab16dfaaae923
julien14 <juxishimwe@gmail.com>
2025-03-19 19:32:42 +0200

    work on the validating blogs add body Errors

--- src/validation/blogValidation.ts
+++ src/validation/blogValidation.ts
@@ -4,16 +4,15 @@ import express from 'express';
 export const validateBlog = [
   body('title').trim().withMessage('Please Enter the title of this article'),
   body('content').trim().withMessage('Please Enter the content of the article'),
+  async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ): Promise<void> => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
 ];
-
-async (
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction
-): Promise<void> => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res.status(400).json({ errors: errors.array() });
-  }
-  next();
-};
