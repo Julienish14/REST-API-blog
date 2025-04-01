6ebd42791bea066ef12686aed998b082ae4aea68
julien14 <juxishimwe@gmail.com>
2025-04-01 13:27:45 +0200

    Work on blog validation

--- src/router/blog.ts
+++ src/router/blog.ts
@@ -8,9 +8,16 @@ import {
 } from '../controllers/Blogs';
 import { isAuthenticated } from '../middlewares';
 import upload from '../utils/multer';
+import { validateBlog } from '../validation/blogValidation';
 
 export default (router: express.Router) => {
-  router.post('/blogs', isAuthenticated, upload.single('image'), createBlog);
+  router.post(
+    '/blogs',
+    isAuthenticated,
+    upload.single('image'),
+    validateBlog,
+    createBlog
+  );
   router.get('/blogs', isAuthenticated, getAllPost);
   router.get('/blogs/:postId', isAuthenticated, getOnePost);
   router.delete('/blogs/:postId', isAuthenticated, deletePost);
