148af641102b62eabe3abe5319d297d1f9e06a1a
julien14 <juxishimwe@gmail.com>
2025-11-05 07:45:25 +0200

    Testing validation

--- src/validation/userValidation.ts
+++ src/validation/userValidation.ts
@@ -56,16 +56,16 @@ export const validateUser: (
       'Password must contain at least one uppercase letter, one lowercase letter, and one number'
     ),
 
-  // body('password')
-  //   .trim()
-  //   .notEmpty()
-  //   .withMessage('Password is required!')
-  //   .isLength({ min: 6 })
-  //   .withMessage('Password must be at least 6 characters long')
-  //   .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/)
-  //   .withMessage(
-  //     'Password must contain at least one uppercase letter, one lowercase letter, and one number'
-  //   ),
+  body('password')
+    .trim()
+    .notEmpty()
+    .withMessage('Password is required!')
+    .isLength({ min: 6 })
+    .withMessage('Password must be at least 6 characters long')
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/)
+    .withMessage(
+      'Password must contain at least one uppercase letter, one lowercase letter, and one number'
+    ),
 
   (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
